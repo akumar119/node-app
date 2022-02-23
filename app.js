a3fa984aa37e6688ad10b23744330196da9526b0
amit <akumar@deqode.com>
2022-02-23 17:30:23 +0530

    add debugger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
+    console.log('hi working...')
     res.send('<h1>Node app</h1>')
 })
 app.listen(PORT, () => {
